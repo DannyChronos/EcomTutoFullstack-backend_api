e21469c5443a1bd30a0dceb4f8365c80267275d2
Danny <Dayane BAKARI>
2025-12-10 17:30:52 +0100

    Add Products Routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 
+// Import Router
+
+import productsRoutes from "./routes/products/index";
+
 const app = express();
 const port = 3000;
+
 
 app.get("/", (req, res) => {
     res.send("Hello World !!");
 });
+
+
+app.use('/products', productsRoutes); // product api routes
+
+
 
 
 app.listen(port, () => {
---
+++ src/routes/products/index.ts
@@ -0,0 +1,18 @@
+import {Router} from "express"
+
+const router = Router();
+// Products endpoints
+
+router.get('/', (req, res) => {
+    
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+export default router;
